4afb3d7c9c4ffa0d14f1047557d6bad29cc908c3
MsBekzod <bekzodmuzaffarovich@example.com>
2024-04-02 20:26:52 +0500

    UYGA VAZIFA: 1-uyga vazifa

--- 1-amaliy vazifa.cs
+++
@@ -1,3 +0,0 @@
-// 1. Doira yuzi nva aylana uzunligini hisoblash.
-
-System.Console.WriteLine("Salom! Men sizga doiraning yuzini va aylana uzunligini xisoblab bera olaman, buning uchun siz doiraning Radiusini kiritsangiz bas !.");
---
+++ Program.cs
@@ -0,0 +1,11 @@
+﻿// 1. Doira yuzi va aylana uzunligini hisoblash.
+System.Console.WriteLine("Assalom alekum, men doira yuzi va aylana uzunligini hisoblab beraman buning uchun siz aylana radiusini kiritsangiz bas ! ");
+System.Console.Write("Doira radiusini kiriting:");
+string radius = Console.ReadLine();
+int r = Convert.ToInt32(radius);
+int pi = 3.14159265;
+int javobL = 3.14159265*2*r;
+int javobS = 3.14159265*r;
+
+System.Console.WriteLine($"Doiraning yuzasi {javobS}, aylananing uzunligi esa {javobL}.");
+
