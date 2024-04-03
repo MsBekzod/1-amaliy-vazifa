9011a651cc46824fad85ce70183280580645cf00
MsBekzod <bekzodmuzaffarovich@example.com>
2024-04-03 18:44:59 +0500

    UYGA VAZIFA: 1-amaliy-vazifa

--- Program.cs
+++ Program.cs
@@ -1,11 +1,20 @@
-﻿// 1. Doira yuzi va aylana uzunligini hisoblash.
-System.Console.WriteLine("Assalom alekum, men doira yuzi va aylana uzunligini hisoblab beraman buning uchun siz aylana radiusini kiritsangiz bas ! ");
-System.Console.Write("Doira radiusini kiriting:");
-string radius = Console.ReadLine();
-int r = Convert.ToInt32(radius);
-int pi = 3.14159265;
-int javobL = 3.14159265*2*r;
-int javobS = 3.14159265*r;
+﻿// // 1. Doira yuzi va aylana uzunligini hisoblash.
 
-System.Console.WriteLine($"Doiraning yuzasi {javobS}, aylananing uzunligi esa {javobL}.");
+// System.Console.WriteLine("Assalom alekum, men doira yuzi va aylana uzunligini hisoblab beraman buning uchun siz aylana radiusini kiritsangiz bas ! ");
+// System.Console.Write("Doira radiusini kiriting:");
+// string radius = Console.ReadLine();
+// int rad = Convert.ToInt32(radius);
+// string pi = ("3.1415");
+// int pii = Convert.ToInt32(pi);
+// int javobL = pii*2*rad;
+// int javobS = rad*pii;
+
+// System.Console.WriteLine($"Doiraning yuzasi {javobS}, aylananing uzunligi esa {javobL}.");
+
+
+
+// 2. Valyuta konvertri.
+
+System.Console.WriteLine("Endi biz bilan Valyuta kursini tezda bilib olishingiz mumkin!!!");
+System.Console.WriteLine();
 
